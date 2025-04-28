506fc17c7d0018779b1d35c3e771bd79d8fab7bd
Luna-irevit <lsy77877korea@gmail.com>
2025-04-28 17:50:03 +0900

    react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,56 @@ import "./App.css";
 import { Tabs } from "@/components/Tabs";
 import { Toaster } from "@/components/Toaster";
 import { ReactNode } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Container>
-      <Tabs.Root defaultValue="overview">
-        <Tabs.List>
-          <Tabs.Trigger value="overview">Overview</Tabs.Trigger>
-          <Tabs.Trigger value="clusters">Clusters</Tabs.Trigger>
-          <Tabs.Trigger value="policies">Policies</Tabs.Trigger>
-        </Tabs.List>
-        <Tabs.Content value="overview">
-          <Overview />
-        </Tabs.Content>
-        <Tabs.Content value="clusters">
-          <Clusters />
-        </Tabs.Content>
-        <Tabs.Content value="policies">
-          <Policies />
-        </Tabs.Content>
-      </Tabs.Root>
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <FederationTabs />
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+function FederationTabs() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const currentTab = (() => {
+    if (location.pathname.startsWith("/clusters")) {
+      return "clusters";
+    }
+    if (location.pathname.startsWith("/policies")) {
+      return "policies";
+    }
+    return "overview";
+  })();
+
+  const handleTabChange = (details: { value: string }) => {
+    navigate(`/${details.value}`);
+  };
+  return (
+    <Tabs.Root value={currentTab} onValueChange={handleTabChange}>
+      <Tabs.List>
+        <Tabs.Trigger value="overview">Overview</Tabs.Trigger>
+        <Tabs.Trigger value="clusters">Clusters</Tabs.Trigger>
+        <Tabs.Trigger value="policies">Policies</Tabs.Trigger>
+      </Tabs.List>
+
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/clusters" element={<Clusters />} />
+        <Route path="/policies" element={<Policies />} />
+      </Routes>
+    </Tabs.Root>
   );
 }
 
