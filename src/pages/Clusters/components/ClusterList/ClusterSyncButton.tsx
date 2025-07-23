77135b1f8265e5f1d886d43594d98ef1b43ea304
230-Luna <lsy77877@gmail.com>
2025-07-23 17:28:28 +0900

    sync 진행중

--- src/pages/Clusters/components/ClusterList/ClusterSyncButton.tsx
+++ src/pages/Clusters/components/ClusterList/ClusterSyncButton.tsx
@@ -6,10 +6,12 @@ import { Sync } from "@/models/sync";
 import {
   Accordion,
   Checkbox,
+  createTreeCollection,
   Drawer,
   HStack,
   Portal,
   Stack,
+  TreeView,
 } from "@chakra-ui/react";
 import {
   useIsMutating,
@@ -22,6 +24,13 @@ import { useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 import { SyncPostBody } from "./../../../../models/sync";
 import { ResourceKindLowercase } from "@/models/resourceModel";
+import {
+  LuChevronRight,
+  LuFile,
+  LuFolder,
+  LuSquareMinus,
+  LuSquarePlus,
+} from "react-icons/lu";
 
 export default function ClusterSyncButton({
   clusterStatus,
@@ -163,6 +172,51 @@ function ClusterResourceSyncDrawer({
     mutationKey: ["handleApplySync", clusterId],
   });
 
+  interface Node {
+    id: string;
+    name: string;
+    children?: Node[];
+  }
+
+  const collection = createTreeCollection<Node>({
+    nodeToValue: (node) => node.id,
+    nodeToString: (node) => node.name,
+    rootNode: {
+      id: "ROOT",
+      name: "",
+      children: [
+        {
+          id: "node_modules",
+          name: "node_modules",
+          children: [
+            { id: "node_modules/zag-js", name: "zag-js" },
+            { id: "node_modules/pandacss", name: "panda" },
+            {
+              id: "node_modules/@types",
+              name: "@types",
+              children: [
+                { id: "node_modules/@types/react", name: "react" },
+                { id: "node_modules/@types/react-dom", name: "react-dom" },
+              ],
+            },
+          ],
+        },
+        {
+          id: "src",
+          name: "src",
+          children: [
+            { id: "src/app.tsx", name: "app.tsx" },
+            { id: "src/index.ts", name: "index.ts" },
+          ],
+        },
+        { id: "panda.config", name: "panda.config.ts" },
+        { id: "package.json", name: "package.json" },
+        { id: "renovate.json", name: "renovate.json" },
+        { id: "readme.md", name: "README.md" },
+      ],
+    },
+  });
+
   return (
     <Portal>
       <Drawer.Backdrop />
@@ -172,6 +226,26 @@ function ClusterResourceSyncDrawer({
             <Drawer.Title>{clusterName}</Drawer.Title>
           </Drawer.Header>
           <Drawer.Body>
+            <TreeView.Root collection={collection} maxW="sm" size="md">
+              <TreeView.Tree>
+                <TreeView.Node
+                  render={({ node, nodeState }) =>
+                    nodeState.isBranch ? (
+                      <TreeView.BranchControl>
+                        {nodeState.expanded ? (
+                          <LuSquareMinus />
+                        ) : (
+                          <LuSquarePlus />
+                        )}
+                        <TreeView.BranchText>{node.name}</TreeView.BranchText>
+                      </TreeView.BranchControl>
+                    ) : (
+                      <TreeView.Item>{node.name}</TreeView.Item>
+                    )
+                  }
+                />
+              </TreeView.Tree>
+            </TreeView.Root>
             <Accordion.Root
               multiple
               value={expandedNamespaces}
